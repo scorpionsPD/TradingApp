500498c147dd3ee1dde5c2710d22ce84d683a6e9
Pradeep Dahiya <pradeepdahiya@businessnext.com>
2023-06-05 15:55:39 +0100

    Initial Commit

---
+++ Trading App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trading App
+//
+//  Created by Pradeep Dahiya on 05/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
